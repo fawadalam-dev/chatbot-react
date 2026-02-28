3f45bce3ff73a9989238028b41a11061f470c40d
Fawad Alam <fawadalam5813@gmail.com>
2026-02-28 15:37:45 +0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -69,7 +69,7 @@ function App() {
             type="submit"
             className="bg-blue-500 text-white p-2 rounded ml-2"
           >
-            Submit
+          Send
           </button>
         </form>
       </SignedIn>
